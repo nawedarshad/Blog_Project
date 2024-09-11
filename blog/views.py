22bb2e57f084a0b432177e8c1c817f51da271b7b
Mohammad Nawed Arshad <nawedarshad25@gmail.com>
2024-09-11 22:31:47 +0530

    Just Started

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
+posts = [
+    {
+    'author':'Nawed Arshad',
+    'title':'My first Blog',
+    'content':'jsncjd cniajdia jdiajdi oamda ncf ojdn vjnsfoas cfo iae jmdolajwmd',
+    'date':'10 Aug 2024'
+    },
+    {
+    'author':'Arshad',
+    'title':'My first Blog',
+    'content':'amda ncf ojdn vjnsfoas cfo iae jmdolajwmd',
+    'date':'12 Aug 2024'
+    }
+]
 def home(request):
-    return render(request, 'Blog/home.html')
+    context = {
+        'posts':posts,
+    }
+    return render(request, 'Blog/home.html', context)
 
 def about(request):
     return render(request, 'Blog/about.html')
